93ab89ec40b4b33524f92e31a38635fe9f1e6a52
Jean-Romain Prévost <jrp@yelp.com>
2014-11-07 13:57:56 -0800

    Add arguments support

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
-var express = require("express");
+var express = require("express"),
+	argv = require("minimist")(process.argv.slice(2)),
+	open = require("open");
 
-var app = express();
-var cwd = process.cwd();
+var app = express(),
+	cwd = process.cwd(),
+	DEFAULT_PORT = 8042,
+	PORT = DEFAULT_PORT;
 
-//app.use(express.logger());
+console.log(argv, process.argv)
+
+if (argv.h || argv.help) {
+	console.log("www.js 0.4.2 - by Jr@ailleurs.me");
+	console.log("");
+	console.log("usage: www.js [-p port] [-loh] [path]");
+	console.log("-p, --port PORT\t<path>\tPort on wich to listen to");
+	console.log("-L, --no-logging\t<path>\tTurn off logging (on )");
+	console.log("-o, --open\t<path>\tTo open a browser to the correct address");
+	console.log("-h, --help\t<path>\tThis help");
+	process.exit();
+}
+
+if (!argv.L && !argv["no-logging"]) {
+	app.use(express.logger('dev'));
+}
+
+if (argv.p || argv.port ) {
+	PORT = argv.port || argv.p;
+	PORT = (Number.isFinite(PORT))? PORT : DEFAULT_PORT;
+}
+
 app.use(express.directory(cwd));
 app.use(express.static(cwd));
 
-app.listen(8042);
+console.log("Start server on port: " + PORT);
+app.listen(PORT);
+
+if(argv.o || argv.open) {
+	open("http://localhost:" + PORT);
+}
